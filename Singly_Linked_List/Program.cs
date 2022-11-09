bc68ce00e9c1acdd004089f0076b508dcb4da36a
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-11-09 16:51:13 +0700

    Add project files.

---
+++ Singly_Linked_List/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singly_Linked_List
+{
+    class Node
+    {
+        public int noMhs;
+        public string nama;
+        public Node next;
+    }
+
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+
+        public void addNode()/
+    }
+}
