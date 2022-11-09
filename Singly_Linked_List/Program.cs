2fd37c2fe80d18d27f1d287458effa96a31cd74a
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-11-09 17:28:57 +0700

    public bool

--- Singly_Linked_List/Program.cs
+++ Singly_Linked_List/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,81 @@ namespace Singly_Linked_List
             START = null;
         }
 
-        public void addNode()/
+        public void addNode()/*Method untuk menanbahkan kedalam list*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+            if (START == null || nim <= START.noMhs)/*Node ditambahkan sebagai node pertama*/
+            {
+                if((START != null) &&  (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiwa sama tidak di ijiknkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+
+            /*Menemukan lokasi node baru didalam list*/
+
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer mahasiwa sama tidak di ijinkan\n");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            /*Node baru akan ditempatkan diantara previous dan current*/
+
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
+        }
+
+        /*Method untuk mengehapus node terntentu didalam list*/
+        public bool delNode(int nim)
+        {
+            Node previous, current;
+            previous = current = null;
+            /*check apakah node yang dimaksud ada dalam list atau tidak*/
+            if(Search(nim, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == START)
+                START = START.next;
+            return true;
+        }
+
+        /*Method untuk mengecheck apakah node yang dimaksud ada dalam list*/
+        public bool Search(int nim, ref Node previous, ref Node current)
+        {
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim != current.noMhs))
+            {
+                previous = current;
+                current = current.next;
+            }
+
+            if (current == null)
+                return (false);
+            else
+                return (true);
+        }
     }
 }
